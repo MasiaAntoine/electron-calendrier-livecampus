a22f4268ca7859f6c54c44cf6df492a59578e4f6
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-30 14:32:43 +0200

    setup

---
+++ src/main/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/renderer/renderer.ts
@@ -0,0 +1,49 @@
+// import { format } from "date-fns";
+
+// // Date actuelle
+// let currentDate = new Date();
+
+// // Fonction pour rendre le calendrier
+// function renderCalendar(date: Date) {
+//   const month = format(date, "MMMM yyyy");
+//   const daysInMonth = new Date(
+//     date.getFullYear(),
+//     date.getMonth() + 1,
+//     0
+//   ).getDate();
+
+//   let calendarHTML = `
+//     <div>
+//       <h1 class="text-2xl">${month}</h1>
+//       <div class="grid grid-cols-7 gap-4 mt-4">`;
+
+//   for (let day = 1; day <= daysInMonth; day++) {
+//     const dayDate = new Date(date.getFullYear(), date.getMonth(), day);
+
+//     calendarHTML += `<div class="border p-2">${day}</div>`;
+//   }
+
+//   calendarHTML += `</div></div>`;
+
+//   const appElement = document.getElementById("app");
+//   if (appElement) {
+//     appElement.innerHTML = calendarHTML;
+//   } else {
+//     console.error("Element with ID 'app' not found");
+//   }
+// }
+
+// // Assurez-vous que le DOM est complètement chargé avant d'exécuter le script
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("DOM fully loaded and parsed");
+//   renderCalendar(currentDate);
+// });
+
+const appElement = document.getElementById("app");
+
+if (appElement) {
+  appElement.innerHTML = "<h1>Salut</h1>";
+  console.log("DOM fully loaded and parsed");
+} else {
+  console.error("Element with ID 'app' not found");
+}
