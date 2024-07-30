dcbeb83f45cb2b75daf1a23426016dcf5303999d
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-30 14:51:44 +0200

    correction preload.ts

---
+++ src/preload.ts
@@ -0,0 +1,28 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
+
+type ValidSendChannels = "toMain" | "saveEvent" | "deleteEvent" | "loadEvents";
+type ValidReceiveChannels = "fromMain" | "eventsLoaded";
+
+type Callback = (...args: any[]) => void;
+
+contextBridge.exposeInMainWorld("electron", {
+  send: (channel: ValidSendChannels, data: any) => {
+    const validChannels: ValidSendChannels[] = [
+      "toMain",
+      "saveEvent",
+      "deleteEvent",
+      "loadEvents",
+    ];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel: ValidReceiveChannels, func: Callback) => {
+    const validChannels: ValidReceiveChannels[] = ["fromMain", "eventsLoaded"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event: IpcRendererEvent, ...args: any[]) =>
+        func(...args)
+      );
+    }
+  },
+});
