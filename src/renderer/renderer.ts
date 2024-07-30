6cd7fa219a96ee9a27de902e60eaa5f351f3a9ea
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-30 18:59:55 +0200

    purge

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,6 +1,5 @@
 import { renderCalendar, currentDate } from "./calendar/calendar";
 
-// Assurez-vous que le DOM est complètement chargé avant d'exécuter le script
 document.addEventListener("DOMContentLoaded", () => {
   renderCalendar(currentDate);
 });
