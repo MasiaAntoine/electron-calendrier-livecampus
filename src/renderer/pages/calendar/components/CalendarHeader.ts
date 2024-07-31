de8d749b51c01b97369f36c3f709cf1cec660810
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-31 11:35:15 +0200

    traduction du mois en français

--- src/renderer/pages/calendar/components/CalendarHeader.ts
+++ src/renderer/pages/calendar/components/CalendarHeader.ts
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 
 export function getMonthHeader(date: Date): string {
-  const month = format(date, "MMMM yyyy");
+  const month = format(date, "MMMM yyyy", { locale: fr });
   return `<h1 class="text-4xl font-medium p-2">${month}</h1>`;
 }
